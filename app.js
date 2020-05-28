de77cb734fd51568e64170c5a0c3a6af03e0b577
Alex Doss <alex.doss42@gmail.com>
2020-05-27 23:48:35 -0600

    add all packages to app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var express = require("express");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
