bfbee97d3a59ac6635d69a96b1b04541a2454938
Alex Doss <alex.doss42@gmail.com>
2020-05-27 23:40:29 -0600

    forgot app.listen

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
   res.render("home");
 });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("Its over Anakin,  I have the high Port")
+});
